c034cf40c2f0d7900b61491bd14cfb86d3f32c6e
Rasujon3 <rasujon3@gmail.com>
2022-02-27 20:28:46 +0600

    details api done

--- app.js
+++ app.js
@@ -8,24 +8,33 @@ const allPlayers = () => {
 };
 
 const showPlayerDetail = (players) => {
-  const parent = document.getElementById("player-container");
   for (const player of players) {
+    const parent = document.getElementById("player-container");
+
     const div = document.createElement("div");
     div.innerHTML = `
       <div class="card border p-5">
         <div class="pro-pic">
-          <img class="w-25" src="" alt="" srcset="" />
+          <img class="w-50" src="${player.strThumb}" />
         </div>
-        <h2>Name:</h2>
-        <h5>COuntry:</h5>
-        <p></p>
+        <h2>Name: ${player.strPlayer}</h2>
+        <h5>Country: ${player.strNationality}</h5>
+        <p>${player.strPlayer}</p>
         <div class="allButton">
           <button class="btn btn-danger">Delete</button>
-          <button class="btn btn-success">Details</button>
+          <button onclick="details(${player.idPlayer})" class="btn btn-success">Details</button>
         </div>
       </div>
     `;
     parent.appendChild(div);
   }
   console.log(players);
+};
+
+const details = (id) => {
+  console.log("id=", id);
+  const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data.players));
 };
